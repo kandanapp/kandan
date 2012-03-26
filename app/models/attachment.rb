e354f7e677df90dcf9100073d62871c0e5b803bb
Akash Manohar J <akash@akash.im>
2012-03-27 01:44:19 +0530

    Uncomments the STS token option
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -8,8 +8,8 @@ class Attachment < ActiveRecord::Base
                     :storage         => :s3,
                     :s3_credentials  => {
                       :access_key_id     => ENV['S3_ACCESS_KEY_ID'],
-                      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']#,
-                      #:session_token     => ENV['STS_SESSION_TOKEN']
+                      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
+                      :session_token     => ENV['STS_SESSION_TOKEN']
                     },
                     :bucket            => ENV['S3_BUCKET'],
                     :url             => "/:attachment/:id/:style/:basename.:extension",
