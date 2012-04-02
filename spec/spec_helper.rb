60150e21d21552b6bb76107c361e132248715133
Akash Manohar J <akash@akash.im>
2012-04-02 23:36:47 +0530

    Specs for API and channels
    * Adds spec for API controller and fixes channels controller spec
    
    * Changes FactoryGirl definitions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require "#{Rails.root}/lib/active_users.rb"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
