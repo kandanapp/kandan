3ef00e076f5af7826cdafdccd01efc2a8925784b
Akash Manohar J <akash@akash.im>
2012-04-10 21:31:04 +0530

    Seperates publishing for messages and uploads
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/models/activity_observer.rb
+++ app/models/activity_observer.rb
@@ -2,12 +2,33 @@ class ActivityObserver < ActiveRecord::Observer
 
   def after_save(activity)
     if activity.action == "message" || activity.action == "upload"
-      faye_channel = "/channels/#{activity.channel.to_param}"
-      broadcast_data = activity.attributes.merge({
-          :user => activity.user.attributes,
-          :channel => activity.channel.attributes
-        })
+      faye_channel, broadcast_data = self.send "#{activity.action}_broadcast_data", activity
       Kandan::Config.broadcaster.broadcast(faye_channel, broadcast_data)
     end
+  end
+
+  private
+  def message_broadcast_data(activity)
+    faye_channel = "/channels/#{activity.channel.to_param}"
+    broadcast_data = activity.attributes.merge({
+        :user => activity.user.attributes,
+        :channel => activity.channel.attributes
+      })
+    [faye_channel, broadcast_data]
+  end
+
+  def upload_broadcast_data(activity)
+    faye_channel = "/app/activities"
+    broadcast_data = {
+      :event  => "attachment#upload",
+      :entity => activity.attributes.merge({
+          :user    => activity.user.attributes,
+          :channel => activity.channel.attributes
+        }),
+      :extra  => {
+        :attachments => activity.channel.attachments.as_json(:methods => :url)
+      }
+    }
+    [faye_channel, broadcast_data]
   end
 end
