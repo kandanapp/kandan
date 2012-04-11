30364e6e73759b7440cf89895839e0ec24b6d844
Sean Grove <s@bushi.do>
2012-04-11 14:07:44 -0700

    Disable rails initialization on asset precompilation for Heroku compatibility

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ end
 
 module Kandan
   class Application < Rails::Application
+    # Dealing with Heroku-specific eccentricities
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
