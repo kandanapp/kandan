ede5d03d3bfa366216f4e0bbf6c96accb22bb23d
Sean Grove <s@bushi.do>
2012-04-11 15:11:18 -0700

    Updates the standard Devise modules (off of Bushido) to match the database migrations

--- config/initializers/kandan.rb
+++ config/initializers/kandan.rb
@@ -2,12 +2,14 @@ Dir["#{Rails.root}/lib/broadcasters/**/*.rb"].each do |file|
   require file
 end
 
-
 module Kandan
   def self.devise_modules
-    standard = [:database_authenticatable, :registerable, :confirmable,
-      :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable]
-    bushido = [:bushido_authenticatable, :trackable, :token_authenticatable]
+
+    standard = [:database_authenticatable, :registerable,
+                :recoverable, :rememberable, :trackable, :validatable]
+
+    bushido = [:bushido_authenticatable, :trackable,
+               :token_authenticatable]
 
     ::Bushido::Platform.on_bushido? ? bushido : standard
   end
