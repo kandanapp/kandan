a8a944262c6f5e088b9890982377025b82fa2f20
Alex Manelis <amanelis@gmail.com>
2013-01-23 19:19:06 -0800

    Slight modification to how the api controller spec is structured

--- spec/controllers/apis_controller_spec.rb
+++ spec/controllers/apis_controller_spec.rb
@@ -1,19 +1,21 @@
 require "spec_helper"
 
 describe ApisController do
-  
-  before :each do
-    @user = FactoryGirl.create(:user)
-    request.env['warden'].stub :authenticate! => @user
-    controller.stub :current_user => @user
-  end
-  
-  
-  describe "active_users" do
-    it "should return json" do
-      get :active_users, :format => :json
-      ActiveUsers.stub!(:all).and_return([])
-      JSON(response.body).should be_kind_of(Array)
+  describe "#active_users" do
+    before do
+      @user = FactoryGirl.create(:user)
+    end
+    context "when a user is authenticated" do
+      before do
+        request.env['warden'].stub :authenticate! => @user
+        controller.stub :current_user => @user
+        get :active_users, :format => :json
+      end
+
+      it "should return json" do
+        ActiveUsers.stub!(:all).and_return([])
+        JSON(response.body).should be_kind_of(Array)
+      end
     end
   end
 end
