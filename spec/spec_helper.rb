9972104e905056ab3b694a4b62d01dea2265be7f
Tony Guntharp <fusion94@damagestudios.net>
2013-03-04 12:00:38 -0800

    adding Coveralls suite Fixes #178

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 SimpleCov.start 'rails'
 
+require 'coveralls'
+Coveralls.wear! 'rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
