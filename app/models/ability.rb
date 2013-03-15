621fdf6c4bbad2346927774b55ecafe21537557e
Mark J. Titorenko <mark.titorenko@alces-software.com>
2013-03-15 23:59:19 +0000

    fix abilities for channel ownership

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,13 @@
-#
+#==============================================================================
 # This file is part of Kandan.
 # https://github.com/kandanapp/kandan
 #
 # Kandan's code and assets are dual-licensed. Kandan is available
 # generally under the AGPL, and also under a custom license via
-# special agreement. See LICENSE for the AGPL terms, and contact us at
-# <admin@kandanapp.com> if you're interested in development of Kandan
-# under a custom license.
-#
+# special agreement. See LICENSE for the AGPL terms, and contact us
+# at <admin@kandanapp.com> if you're interested in development of
+# Kandan under a custom license.
+#==============================================================================
 class Ability
   include CanCan::Ability
 
@@ -16,7 +16,10 @@ class Ability
       can :manage, :all
     else
       can [:read, :create], Channel
+      can :manage, Channel, :user => user
+      can :manage, User, :id => user.id # can manage themselves
     end
+    can :read, User
     # This goes last in order to override all other permissions.
     cannot :destroy, Channel, :id => 1
   end
