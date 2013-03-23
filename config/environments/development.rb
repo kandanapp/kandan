73f7deb2f556a61ac84559931ed610a752b4641f
Gabriel Cebrian <gabceb@gmail.com>
2013-03-23 13:07:56 -0700

    Remove unnecessary log from development file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,4 @@ Kandan::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.logger = Logger.new(STDOUT)
-  config.logger.level = Logger.const_get(
-    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
-  )
-  
 end
