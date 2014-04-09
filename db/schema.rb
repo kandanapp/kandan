8558eb51f19b3ae80475e92e1ff9eaa6761487e8
Scott Goci <scottjg@gmail.com>
2014-04-09 15:33:49 -0400

    Fixed current user list not being updated when user logs in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623202749) do
+ActiveRecord::Schema.define(:version => 20140409193213) do
 
   create_table "activities", :force => true do |t|
     t.text     "content"
