4c8762dd6e867288f0979d16322df17d9697eec2
Scott Goci <scottjg@gmail.com>
2014-04-18 20:31:37 -0400

    Fixing additional xss vulnerabilities and removing User authentication token

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,14 @@ module ApplicationHelper
   def current_user_data
     current_user_data = {
       :id            => current_user.id,
-      :first_name    => current_user.first_name,
-      :last_name     => current_user.last_name,
+      :first_name    => sanitize(current_user.first_name),
+      :last_name     => sanitize(current_user.last_name),
       :email         => current_user.email,
-      :username      => current_user.username,
+      :username      => sanitize(current_user.username),
       :auth_token    => current_user.authentication_token,
       :gravatar_hash => current_user.gravatar_hash,
-      :avatar_url    => current_user.avatar_url
+      :avatar_url    => current_user.avatar_url,
+      :is_admin      => current_user.is_admin
     }
   end
 end
---
+++ config/initializers/json_escape.rb
@@ -0,0 +1,8 @@
+class ActionView::Base
+  def json_escape(s)
+    result = s.to_s.gsub('/', '\/')
+    s.html_safe? ? result.html_safe : result
+  end
+
+  alias j json_escape
+end
