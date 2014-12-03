0ba54f23f8cf0b3888d5e70c4b3bc0ad746abfd1
Scott Goci <scottjg@gmail.com>
2014-12-03 12:47:26 -0500

    Fixed a few wording issues, tweaked how users sign in / show suspended / show approved and fixed password reset feature that Devise 3.1 broke

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,21 @@ class ApplicationController < ActionController::Base
   def force_approved_account
     # We will redirect to the approval page if a user is signed in, is not an admin and is marked as waiting for approval
     redirect = user_signed_in? && !current_user.is_admin? && current_user.registration_status.waiting_approval?
-
-    redirect_to approval_path if redirect
+    if redirect
+      flash[:notice] = "Your account needs to be approved by an administrator before you can start using the Kandan app."
+      sign_out current_user
+      new_user_session_path
+    end
   end
 
   def redirect_suspended_account
     # We will redirect to suspended if a user is signed in and its marked as suspended 
     redirect = user_signed_in? && current_user.registration_status.suspended?
-    
-    redirect_to suspended_path if redirect
+    if redirect
+      flash[:notice] = "Sorry, the administrator of this site has suspended your account."
+      sign_out current_user
+      new_user_session_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,28 +3,35 @@ Kandan::Application.routes.draw do
   devise_for :users, :controllers => {
     :sessions => "sessions"
   }
+  devise_scope :user do
+    authenticated :user do
+      root :to => "main#index"
 
-  root :to => "main#index"
+      get '/search' => "main#search"
 
-  get '/search' => "main#search"
+      resources :channels do
+        resources :activities
+        resources :attachments
+      end
 
-  resources :channels do
-    resources :activities
-    resources :attachments
-  end
+      resources :users, :only => [:index, :show]
 
-  resources :users, :only => [:index, :show]
+      get "/active_users" => "apis#active_users"
+      get "/me" => "apis#me"
 
-  get "/active_users" => "apis#active_users"
-  get "/me" => "apis#me"
+      get "/users/edit" =>"main#users_edit"
 
-  get "/users/edit" =>"main#users_edit"
+      namespace :admin do
+        root :to => "admin#index"
+        post "/update", :to => "admin#update", :as => "update"
+        post "/update_user", :to => "admin#update_user", :as => "update_user"
+        post "/toggle_admin", :to => "admin#toggle_admin"
+      end
+    end
 
-  namespace :admin do
-    root :to => "admin#index"
-    post "/update", :to => "admin#update", :as => "update"
-    post "/update_user", :to => "admin#update_user", :as => "update_user"
-    post "/toggle_admin", :to => "admin#toggle_admin"
+    unauthenticated do
+      root to: "sessions#new"
+    end
   end
 
   # Pages Controller
