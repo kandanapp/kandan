ca3e6897d6c631aa2a6e1c709a6146c67322d2ab
Hiroshi Miura <miurahr@linux.com>
2015-04-19 09:38:02 +0900

    supress i18n warning
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,7 @@ module Kandan
     # config.i18n.default_locale = :de
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :en
+    config.i18n.enforce_available_locales = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
